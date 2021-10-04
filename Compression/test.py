530a13b9ecf9b901f7f1908087d54236bbb61164
theodorepsi <88879534+theodorepsi@users.noreply.github.com>
2021-10-04 15:05:13 +0200

    Delete Compression directory

--- Compression/test.py
+++
@@ -1,9 +0,0 @@
-import zlib
-import binascii
-
-data = 'Hello world'
-data = bytes(data, 'utf-8')
-compressed_data = zlib.compress(data, 2)
-
-print('Original data: '+ data.decode('utf-8'))
-print('Compressed data: '+ binascii.hexlify(compressed_data).decode('utf-8'))

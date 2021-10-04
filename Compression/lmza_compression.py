47bafc28f4b0760f05d4e7958c6417db5a01887f
theodorepsi <88879534+theodorepsi@users.noreply.github.com>
2021-10-04 15:19:37 +0200

    Update lmza_compression.py

--- Compression/lmza_compression.py
+++ Compression/lmza_compression.py
@@ -2,7 +2,7 @@ import lzma, os, time
 lzc = lzma.LZMACompressor()
 
 # cat /usr/share/dict/words | sort -R | head -c 1MB > data
-filename_in = "9.csv"
+filename_in = "../Testing Data Sets/2018-09-19-03_57_11_VN100.csv"
 filename_out = "compressed_data4.xz"
 
 start = time.time()

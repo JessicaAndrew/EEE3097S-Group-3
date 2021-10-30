5159b7751aa9101354cdbfec73f74c37939ea7cf
JessicaAndrew <52384716+JessicaAndrew@users.noreply.github.com>
2021-10-30 13:39:02 +0200

    Add files via upload

---
+++ Combined/DecryptDecompress.py
@@ -0,0 +1,38 @@
+# note this decryption code was adapted for use from the code on the website: https://kentjuno.com/learning/python/encrypt-file-with-aes-in-python/?__cf_chl_managed_tk__=pmd_hlkUY4MSLokfXLP0W7X5pldoV.w7AWDdhgcb_gkkEJM-1633346584-0-gqNtZGzNAuWjcnBszROl
+from Crypto import Random
+from Crypto.Cipher import AES
+from Crypto.Hash import SHA256
+import os
+import time
+import psutil
+import bz2
+import sys
+
+# decrypt file
+def decrypt(fileName, key):
+    f = open(fileName,'r+b')
+    fileData = f.read()
+    f.close()
+    
+    iv = fileData[:AES.block_size] # retrieve the IV used for encyption
+    cipher = AES.new(key, AES.MODE_CBC, iv) # create the cipher using the AES library
+    decyptedText = cipher.decrypt(fileData[AES.block_size:]) # decrypt the section of the file
+    dec = decyptedText.rstrip(b"\0")
+    
+    w = open(fileName.replace(".bz2", "_d.bz2"),'w+b')
+    w.write(dec)
+    w.close()
+
+# using the password to make a key for the encyption
+def getKey(password):
+    return (SHA256.new(password.encode())).digest()
+
+def main():    
+    filename = str(sys.argv[1])
+    password = input("Password: ")
+    decrypt(filename, getKey(password))
+    inputParameter = "bzip2 -dk "+ filename.replace(".bz2", "_d.bz2")
+    os.system(inputParameter)
+ 
+if __name__ == "__main__":
+    main()
---
+++ Combined/TestLostData.py
@@ -0,0 +1,41 @@
+import filecmp
+import sys
+
+f1 = str(sys.argv[1])
+f2 = f1.replace(".csv", "_c_e_d")
+
+result = filecmp.cmp(f1, f2, shallow=True)
+print("The result will return True if the files match and False if they do not:")
+print("Shallow match: ", result)
+
+
+# reading files
+f1 = open(f1, "r")  
+f2 = open(f2, "r")  
+
+i = 0
+boolean = True
+
+for line1 in f1:
+	i += 1
+	
+	for line2 in f2:
+		
+		# matching line1 from both files
+		if line1 == line2:
+			# print IDENTICAL if similar
+			# print("Line ", i, ": IDENTICAL")
+			boolean = True	
+		else:
+			print("Line ", i, ":")
+			# else print that line from both files
+			print("\tFile 1:", line1, end='')
+			print("\tFile 2:", line2, end='')
+			boolean = False
+		break
+
+# closing files
+f1.close()									
+f2.close()									
+
+print("Deep match: ", boolean)
